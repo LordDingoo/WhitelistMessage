c1d9c153cf9b1a9acfb1576c69ce4726dd057fc7
LordDingoo <d.f.martinsen@gmail.com>
2023-02-22 22:09:39 +0100

    Changed code

--- src/main/java/io/github/lorddingoo/whitelistmessage/ModMessage/MixinPlayerManager.java
+++ src/main/java/io/github/lorddingoo/whitelistmessage/ModMessage/MixinPlayerManager.java
@@ -1,8 +1,11 @@
 package io.github.lorddingoo.whitelistmessage.ModMessage;
 
 import net.minecraft.server.PlayerManager;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -11,6 +14,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class MixinPlayerManager {
     @Redirect(method = "checkCanJoin", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;)Lnet/minecraft/text/MutableText;", ordinal = 0))
     public MutableText mod$redirectWhitelistText(String key) {
-        return Text.literal("§cHello there, you aren't whitelisted yet - join the discord server and make sure to type in your username https://discord.gg/EPWF5A6v2K");
+        return Text.of(Formatting.RED, "Hello there, you aren't whitelisted yet - join the ",
+                Text.of(Formatting.BLUE, "Discord server").styled(style -> style.withClickEvent(
+                        new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/EPWF5A6v2K")
+                )),
+                " and make sure to type in your username"
+        );
     }
 }
