5d33b1db064776c216c42a5d5c469d80a6e06038
LordDingoo <d.f.martinsen@gmail.com>
2023-02-22 18:07:46 +0100

    Initial commit

---
+++ src/main/java/io/github/lorddingoo/whitelistmessage/WhitelistMessage.java
@@ -0,0 +1,10 @@
+package io.github.lorddingoo.whitelistmessage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WhitelistMessage implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/lorddingoo/whitelistmessage/client/WhitelistMessageClient.java
@@ -0,0 +1,13 @@
+package io.github.lorddingoo.whitelistmessage.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WhitelistMessageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
